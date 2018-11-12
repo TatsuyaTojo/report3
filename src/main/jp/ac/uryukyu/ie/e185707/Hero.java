291e38f618fc799a29dbc92b3c0832cb40616c45
Tatsuya TOJO <e185707@ie.u-ryukyu.ac.jp>
2018-11-13 00:22:58 +0900

    Step2

---
+++ src/main/jp/ac/uryukyu/ie/e185707/Hero.java
@@ -0,0 +1,16 @@
+package jp.ac.uryukyu.ie.e185707;
+
+public class Hero extends LivingThing{
+    Hero(String name, int maximumHP, int attack){
+        super(name, maximumHP, attack);
+    }
+    @Override
+    public void wounded(int damage){
+
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+        }
+    }
+}
